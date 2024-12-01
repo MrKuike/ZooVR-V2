a7d39fca7326e9b504bc02703a58001c94d8fff0
MrKuike <natanaelromilio@gmail.com>
2024-12-01 20:12:14 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const panel_pradera = document.querySelector('#ambient-pradera');
+const panel_oceano = document.querySelector('#ambient-oceano');
+const panel_desierto = document.querySelector('#ambient-desierto');
+const panel_sabana = document.querySelector('#ambient-sabana');
+
+panel_pradera.addEventListener('click',()=>{
+    window.location.href = 'escenas/test.html'
+});
+
+panel_oceano.addEventListener('click',()=>{
+    window.location.href = 'escenas/test.html'
+});
+
+panel_desierto.addEventListener('click',()=>{
+    window.location.href = 'escenas/test.html'
+});
+
+panel_sabana.addEventListener('click',()=>{
+    window.location.href = 'escenas/test.html'
+});
